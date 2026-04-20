04b00185ad3570814e7a1851f2da8ba8b131eaec
NXYorz <niexiaoyang12138@126.com>
2026-04-21 01:05:00 +0800

    Init + add config + add definition

---
+++ code/agent_diy/conf/conf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+###########################################################################
+# Copyright © 1998 - 2026 Tencent. All Rights Reserved.
+###########################################################################
+"""
+Author: NXY
+"""
+
+
+import numpy as np
+
+
+# Configuration
+# 配置，包含维度设置，算法参数设置，文件的最后一些配置是开悟平台使用不要改动
+class Config:
+
+   
+    #特征向量，50D
+    FEATURE = [
+        10,#英雄主特征,当前X坐标，Y坐标、闪现是否可用，BUFF,当前分数进展、、已经进入第几个阶段、最近是否卡住、最近是否在重复绕路，是否危险，该贪还是保
+        6,#宝箱特征,宝箱是否还有效、和自己相对方向、相对距离、当前危险度，位置
+        10,#怪物特征,是否可见，位置，速度，距离，dist_norm(两只怪物)
+        8,#合法动作掩码(往哪里走更安全)
+        16,#局部地图特征
+    ]
+    # Whether to use CNN networks
+    # 是否使用CNN网络
+    USE_CNN = True
+    VIEW_SIZE = 50 if USE_CNN else 0
+
+    FEATURE_VECTOR_SHAPE = FEATURES
+    FEATURE_IMAGE_SHAPE = (4, VIEW_SIZE + 1, VIEW_SIZE + 1)
+
+    ACTION_SHAPE = (8,)
+    VALUE_SHAPE = (1,)
+    FEATURE_LEN = sum(FEATURE_SPLIT_SHAPE)
+    DIM_OF_OBSERVATION = FEATURE_LEN
+
+    # Discount factor GAMMA in RL
+    # RL中的回报折扣GAMMA
+    #GAMMA = 0.95
+
+    # Initial learning rate
+    # 初始的学习率
+    #START_LR = 5e-4
+
+    #VALUE_LOSS_COEFF = 0.5
+    #ENTROPY_LOSS_COEFF = 0.025
+
+    # Action space / 动作空间：8个移动方向
+    ACTION_NUM = 8
+
+    # Value head / 价值头：单头生存奖励
+    VALUE_NUM = 1
+
+    # PPO hyperparameters / PPO 超参数
+    GAMMA = 0.995
+    LAMDA = 0.95
+    INIT_LEARNING_RATE_START = 0.0003
+    BETA_START = 0.001
+    CLIP_PARAM = 0.2
+    VF_COEF = 1.0
+    GRAD_CLIP_RANGE = 0.5
